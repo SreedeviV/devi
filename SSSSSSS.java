5f2cdc9ebf88a826c3b9b2bacbb77aa7b2b1f97c
SreedeviV <sreedevi2996@gmail.com>
2017-06-16 14:24:30 +0530

    Add files via upload

---
+++ SSSSSSS.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+
+public class SSSSSSS {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,b,c;
+		System.out.println("Enter the first number");
+		Scanner s=new Scanner(System.in);
+		a=s.nextInt();
+		System.out.println("Enter the second number");
+		b=s.nextInt();
+		System.out.println("The subtraction is");
+		c=a-b;
+		System.out.println(c);
+
+
+	}
+
+
+	}
+
+
