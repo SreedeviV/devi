e5343174fef96c99ee700168b71bd15956eb81b8
SreedeviV <sreedevi2996@gmail.com>
2017-06-20 10:16:36 +0530

    Add files via upload

---
+++ rever.java
@@ -0,0 +1,22 @@
+package beginner;
+
+public class rever {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int rem;
+		int n=68;
+		System.out.println("the reverse number is");
+while(n>0)
+{
+	 rem=n%10;
+	n=n/10;
+	System.out.print(rem);
+}
+	}
+
+}
+
