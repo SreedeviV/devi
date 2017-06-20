dfa6f1ffed28eb0c085824a74347bc07022856ae
SreedeviV <sreedevi2996@gmail.com>
2017-06-20 10:03:19 +0530

    Add files via upload

---
+++ Fact.java
@@ -0,0 +1,24 @@
+package beginner;
+
+public class Fact {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=5;
+		int i;
+		int factorial=1;
+for(i=1;i<=n;i++)
+{
+	factorial=factorial*i;
+}
+System.out.println("the factorial is"+factorial);
+	}
+
+
+
+	}
+
+
