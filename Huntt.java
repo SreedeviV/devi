1dc0ae975023d6e450f24cda7f2d7b21084f607e
SreedeviV <sreedevi2996@gmail.com>
2017-07-04 10:07:26 +0530

    Add files via upload

---
+++ Huntt.java
@@ -0,0 +1,20 @@
+package guvi;
+
+public class Huntt {
+
+	
+		
+
+			Huntt(){
+				System.out.println("Bike is created");
+				
+			}
+			public static void main(String[] args) {
+				// TODO Auto-generated method stub
+		Huntt v=new Huntt();
+			}
+
+		}
+	
+
+
